f92e071ac4293ff1836e952b801308d73221b2d4
Lais Almeida <laisdev25@gmail.com>
2024-06-28 15:58:31 -0300

    Valida campos preenchidos corretamente e data deve ser futura

--- app/models/base_fee.rb
+++ app/models/base_fee.rb
@@ -5,6 +5,7 @@ class BaseFee < ApplicationRecord
 
   accepts_nested_attributes_for :values
   validates :name, :description, :late_payment, :late_fee, :charge_day, presence: true
+  validate :date_is_future?
 
   enum recurrence: {
     monthly: 0,
@@ -16,9 +17,13 @@ class BaseFee < ApplicationRecord
 
   def value_builder
     @values = []
-    self.condo.unit_types.each do |ut|
-      @values << self.values.build(unit_type: ut)
+    condo.unit_types.each do |ut|
+      @values << values.build(unit_type: ut)
     end
     @values
+  end
+
+  def date_is_future?
+    errors.add(:charge_day, :future_date) if charge_day.present? && charge_day <= Time.zone.today
   end
 end
--- spec/models/base_fee_spec.rb
+++ spec/models/base_fee_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe BaseFee, type: :model do
-  describe 'valid?' do
-    context 'presence' do
-      it 'validates presence' do
-        base_fee = BaseFee.create(name: '', description: '', late_payment: '',
-                                  late_fee: '', charge_day: '')
+  describe 'válido?' do
+    context 'presença' do
+      it 'valida presença' do
+        base_fee = build(:base_fee, name: '', description: '', late_payment: '',
+                                    late_fee: '', charge_day: '')
 
         expect(base_fee).not_to be_valid
         expect(base_fee.errors).to include(:name)
@@ -13,6 +13,16 @@ RSpec.describe BaseFee, type: :model do
         expect(base_fee.errors).to include(:late_payment)
         expect(base_fee.errors).to include(:late_fee)
         expect(base_fee.errors).to include(:charge_day)
+      end
+    end
+
+    context 'data' do
+      it 'data deve ser futura' do
+        base_fee = build(:base_fee, charge_day: 1.day.ago)
+
+        expect(base_fee).not_to be_valid
+        expect(base_fee.errors).to include(:charge_day)
+        expect(base_fee.errors[:charge_day]).to include('deve ser futura')
       end
     end
   end
