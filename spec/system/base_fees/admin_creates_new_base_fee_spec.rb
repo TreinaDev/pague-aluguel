c0d804701901516758d457092761238ce619baaf
Lais Almeida <laisdev25@gmail.com>
2024-06-28 16:27:09 -0300

    retorna feedback de erro quando data é passado

--- spec/system/base_fees/admin_creates_new_base_fee_spec.rb
+++ spec/system/base_fees/admin_creates_new_base_fee_spec.rb
@@ -15,11 +15,11 @@ describe 'admin cria taxa fixa' do
     condo = create(:condo, name: 'Prédio lindo', city: 'Cidade maravilhosa')
 
     create(:unit_type, description: 'Apartamento 1 quarto', area: 30,
-           condo: condo)
+                       condo:)
     create(:unit_type, description: 'Apartamento 2 quartos', area: 45,
-           condo: condo)
+                       condo:)
     create(:unit_type, description: 'Apartamento 3 quartos', area: 60,
-           condo: condo)
+                       condo:)
 
     formatted_date = 10.days.from_now.to_date
 
@@ -31,7 +31,7 @@ describe 'admin cria taxa fixa' do
       fill_in 'Valor para Apartamento 1 quarto', with: 200
       fill_in 'Valor para Apartamento 2 quartos', with: 300
       fill_in 'Valor para Apartamento 3 quartos', with: 500
-      select 'Mensal', from: 'Recorrência'
+      select 'Bimestral', from: 'Recorrência'
       fill_in 'Data de Lançamento', with: formatted_date.to_s
       check 'Taxa fixa'
       fill_in 'Juros ao dia', with: 1
@@ -42,8 +42,8 @@ describe 'admin cria taxa fixa' do
     base_fee = BaseFee.last
     expect(page).to have_content 'Taxa cadastrada com sucesso!'
     expect(page).to have_content 'Taxa de Condomínio'
-    expect(page).to have_content 'Recorrência: Mensal'
-    expect(page).to have_content "Data de Lançamento: #{I18n.localize(formatted_date)}"
+    expect(page).to have_content 'Recorrência: Bimestral'
+    expect(page).to have_content "Data de Lançamento: #{I18n.l(formatted_date)}"
     expect(page).to have_content 'Taxa fixa'
     expect(page).to have_content 'Juros de 1% ao dia'
     expect(page).to have_content 'Multa de R$30 por atraso'
@@ -56,11 +56,11 @@ describe 'admin cria taxa fixa' do
     condo = create(:condo, name: 'Prédio lindo', city: 'Cidade maravilhosa')
 
     create(:unit_type, description: 'Apartamento 1 quarto', area: 30,
-           condo: condo)
+                       condo:)
     create(:unit_type, description: 'Apartamento 2 quartos', area: 45,
-           condo: condo)
+                       condo:)
     create(:unit_type, description: 'Apartamento 3 quartos', area: 60,
-           condo: condo)
+                       condo:)
 
     login_as admin, scope: :admin
     visit new_condo_base_fee_path(condo)
@@ -84,5 +84,43 @@ describe 'admin cria taxa fixa' do
     expect(page).to have_content 'Data de Lançamento não pode ficar em branco'
     expect(page).to have_content 'Juros ao dia não pode ficar em branco'
     expect(page).to have_content 'Multa por atraso não pode ficar em branco'
+  end
+
+  it 'e data deve ser futura' do
+    admin = create(:admin, email: 'admin@email.com', password: '123456')
+    condo = create(:condo, name: 'Prédio lindo', city: 'Cidade maravilhosa')
+
+    create(:unit_type, description: 'Apartamento 1 quarto', area: 30, condo:)
+    create(:unit_type, description: 'Apartamento 2 quartos', area: 45, condo:)
+    create(:unit_type, description: 'Apartamento 3 quartos', area: 60, condo:)
+
+    formatted_date = 10.days.ago.to_date
+
+    login_as admin, scope: :admin
+    visit new_condo_base_fee_path(condo)
+    within 'form' do
+      fill_in 'Nome', with: 'Taxa de Condomínio'
+      fill_in 'Descrição', with: 'Taxas mensais para manutenção do prédio.'
+      fill_in 'Valor para Apartamento 1 quarto', with: 200
+      fill_in 'Valor para Apartamento 2 quartos', with: 300
+      fill_in 'Valor para Apartamento 3 quartos', with: 500
+      select 'Semestral', from: 'Recorrência'
+      fill_in 'Data de Lançamento', with: formatted_date.to_s
+      check 'Taxa fixa'
+      fill_in 'Juros ao dia', with: 1
+      fill_in 'Multa por atraso', with: 30
+      click_on 'Salvar'
+    end
+
+    expect(page).to have_content 'Taxa não cadastrada.'
+    expect(page).to have_content 'Data de Lançamento deve ser futura'
+    expect(page).to have_field 'Nome', with: 'Taxa de Condomínio'
+    expect(page).to have_field 'Descrição', with: 'Taxas mensais para manutenção do prédio.'
+    expect(page).to have_field 'Valor para Apartamento 1 quarto', with: 200
+    expect(page).to have_field 'Valor para Apartamento 2 quartos', with: 300
+    expect(page).to have_field 'Valor para Apartamento 3 quartos', with: 500
+    expect(page).to have_checked_field 'Taxa fixa'
+    expect(page).to have_field 'Juros ao dia', with: 1
+    expect(page).to have_field 'Multa por atraso', with: 30
   end
 end
