7f0f713664283436ef5f18f36ddfe104f7e26d60
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-06-28 07:45:32 -0300

    Atualiza o Seed
    Co-authored-by: Matheus Bellucio <mbelluciocoding@gmail.com>
    Co-authored-by: Nathanael Vieira <tmntsplinter04@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -12,11 +12,11 @@ condo = Condo.create!(name: 'Sai de baixo', city: 'Rio de Janeiro')
 second_condo = Condo.create!(name: 'Segundo Condomínio', city: 'Rio de Janeiro')
 
 CommonArea.find_or_create_by!(name: 'Churrasqueira', description: 'Área de churrasqueira com piscina', max_capacity: 30,
-                   usage_rules: 'Regras ainda não definidas', fee: 250, condo:)
+                              usage_rules: 'Regras ainda não definidas', fee: 250, condo:)
 CommonArea.find_or_create_by!(name: 'Play', description: 'Play para eventos', max_capacity: 60,
-                   usage_rules: 'Regras ainda não definidas', fee: 300, condo:)
+                              usage_rules: 'Regras ainda não definidas', fee: 300, condo:)
 
-CommonArea.find_or_create_by!(name: 'Thundercats', description: 'Gatos Selvagens', max_capacity: 40,
-                   usage_rules: 'Não pode dirigir o tanque dentro do salão', fee: 400, condo: second_condo)
-CommonArea.find_or_create_by!(name: 'Dragon Ball Z', description: 'Guerreros Saiajens', max_capacity: 60,
-                   usage_rules: 'Ressucitar o Kuririn.', fee: 500, condo: second_condo)
+CommonArea.find_or_create_by!(name: 'Salão de festa', description: 'Área feita para eventos casuais', max_capacity: 40,
+                              usage_rules: 'Proibido levar as mesas para fora do salão.', fee: 400, condo: second_condo)
+CommonArea.find_or_create_by!(name: 'Cinema', description: 'Guerreros Saiajens', max_capacity: 60,
+                              usage_rules: 'Proibido fumar na sala', fee: 500, condo: second_condo)
