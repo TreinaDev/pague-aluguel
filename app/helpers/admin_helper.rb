ba028632a5a56d43d0b335435110eb4f432592f7
kanzakisuemi <kanzakisuemi@edu.unifil.br>
2024-07-01 20:29:03 -0300

    cria helper p avatar e muda config do rails

--- app/helpers/admin_helper.rb
+++ app/helpers/admin_helper.rb
@@ -1,5 +1,15 @@
 module AdminHelper
+
   def capitalized_name(admin)
     "#{admin.first_name.titleize} #{admin.last_name.titleize}"
   end
+
+  def avatar(admin)
+    if admin.photo.attached?
+      image_tag admin.photo, class: 'avatar me-3'
+    else
+      image_tag 'psycho-user', class: 'avatar me-3'
+    end
+  end
+  
 end
