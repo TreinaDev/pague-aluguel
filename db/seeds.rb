f64fe02cf3221614e0b010e36a5824f54d109bba
sabinopa <priasabino@gmail.com>
2024-07-04 11:29:57 -0300

    inicia traduçã das views

--- db/seeds.rb
+++ db/seeds.rb
@@ -39,14 +39,14 @@ Admin.create!(
 p "Created #{Admin.count} admins"
 
 CommonArea.find_or_create_by!(name: 'Churrasqueira', description: 'Área de churrasqueira com piscina', max_capacity: 30,
-                              usage_rules: 'Regras ainda não definidas', fee_cents: 250)
+                              usage_rules: 'Regras ainda não definidas', fee_cents: 250, condo_id: 20)
 CommonArea.find_or_create_by!(name: 'Play', description: 'Play para eventos', max_capacity: 60,
-                              usage_rules: 'Regras ainda não definidas', fee_cents: 300)
+                              usage_rules: 'Regras ainda não definidas', fee_cents: 300, condo_id: 20)
 
 CommonArea.find_or_create_by!(name: 'Salão de festa', description: 'Área feita para eventos casuais', max_capacity: 40,
-                              usage_rules: 'Proibido levar as mesas para fora do salão.', fee_cents: 400)
+                              usage_rules: 'Proibido levar as mesas para fora do salão.', fee_cents: 400, condo_id: 20)
 CommonArea.find_or_create_by!(name: 'Cinema', description: 'Guerreiros Saiajens', max_capacity: 60,
-                              usage_rules: 'Proibido fumar na sala', fee_cents: 500)
+                              usage_rules: 'Proibido fumar na sala', fee_cents: 500, condo_id: 20)
 
 p "Created #{Admin.count} admins"
 
@@ -56,7 +56,7 @@ base_fee1 = BaseFee.create!(name: 'Taxa de Condomínio',
                             description: 'Manutenção regular do prédio',
                             interest_rate: 2, late_fine: 10, fixed: true,
                             charge_day: 25.days.from_now,
-                            recurrence: :monthly, condo_id: 1)
+                            recurrence: :monthly, condo_id: 20)
             Value.create!(price: 200, unit_type_id: 1, base_fee: base_fee1)
             Value.create!(price: 200, unit_type_id: 2, base_fee: base_fee1)
 
@@ -64,7 +64,7 @@ base_fee2 = BaseFee.create!(name: 'Fundo de Reserva',
                             description: 'Destinado a cobrir despesas imprevistas',
                             interest_rate: 1, late_fine: 5, fixed: true,
                             charge_day: 5.days.from_now,
-                            recurrence: :bimonthly, condo_id: 1)
+                            recurrence: :bimonthly, condo_id: 20)
             Value.create!(price: 300, unit_type_id: 1, base_fee: base_fee2)
             Value.create!(price: 300, unit_type_id: 2, base_fee: base_fee2)
 
