25224a8e5aee844d850b9f475c020d9b73f11aa3
Lais Almeida <laisdev25@gmail.com>
2024-07-05 17:53:26 -0300

    Refatora o seeds.
    Co-authored-by: Angelo Maia <request.angelo@gmail.com>
    Co-authored-by: Lais Almeida <laisdev25@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -43,8 +43,8 @@ base_fee1 = BaseFee.create!(name: 'Taxa de Condomínio',
 
 base_fee2 = BaseFee.create!(name: 'Fundo de Reserva',
                             description: 'Destinado a cobrir despesas imprevistas',
-                            interest_rate: 1, late_fine: 5, limited: false,
-                            charge_day: 5.days.from_now,
+                            interest_rate: 1, late_fine: 5, limited: true,
+                            charge_day: 5.days.from_now, installments: 10,
                             recurrence: :bimonthly, condo_id: 1)
             Value.create!(price: 300, unit_type_id: 1, base_fee: base_fee2)
             Value.create!(price: 300, unit_type_id: 2, base_fee: base_fee2)
