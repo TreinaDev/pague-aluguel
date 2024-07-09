fa69e32f06a14d613441b20460fec9c806122afe
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-07-09 12:04:07 -0300

    Refatoração da Views
    Co-authored-by: Arthur Scortegagna <arthurscortegagna@gmail.com>

--- app/models/common_area_fee.rb
+++ app/models/common_area_fee.rb
@@ -6,6 +6,6 @@ class CommonAreaFee < ApplicationRecord
   monetize :value_cents,
            allow_nil: false,
            numericality: {
-             greater_than_or_equal_to: 0 # Para aceitar cadastrar uma taxa zero
+             greater_than_or_equal_to: 0
            }
 end
