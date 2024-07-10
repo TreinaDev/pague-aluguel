89c99f07ba0660207400b3c69be1fb01c17e3260
Nathanael Vieira <tmntsplinter04@gmail.com>
2024-07-10 18:33:07 -0300

    [WIP] Cria validações para o teste de model.
    Co-authored-by: Angelo Maia <request.angelo@gmail.com>

--- app/models/single_charge.rb
+++ app/models/single_charge.rb
@@ -1,10 +1,13 @@
 class SingleCharge < ApplicationRecord
   validates :unit_id, :issue_date, presence: true
+  validate :common_area_is_mandatory
+  validate :date_is_future
+  validate :description_is_mandatory
 
   monetize :value_cents,
            allow_nil: false,
            numericality: {
-             greater_than_or_equal_to: 0
+             greater_than: 0
            }
 
   enum charge_type: {
@@ -12,4 +15,24 @@ class SingleCharge < ApplicationRecord
     common_area_fee: 2,
     other: 5
   }
+
+  private
+
+  def common_area_is_mandatory
+    return unless charge_type == 'common_area_fee' && common_area_id.nil?
+
+    errors.add(:common_area_id, 'deve ser selecionada.')
+  end
+
+  def date_is_future
+    return unless issue_date.present? && issue_date < Time.zone.today
+
+    errors.add(:issue_date, 'deve ser a partir de hoje.')
+  end
+
+  def description_is_mandatory
+    return unless charge_type != 'common_area_fee' && description.nil?
+
+    errors.add(:description, 'não pode ficar em branco.')
+  end
 end
--- spec/models/single_charge_spec.rb
+++ spec/models/single_charge_spec.rb
@@ -1,5 +1,46 @@
 require 'rails_helper'
 
 RSpec.describe SingleCharge, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#valid?' do
+    it { should validate_presence_of(:unit_id) }
+    it { should validate_presence_of(:issue_date) }
+    it { should validate_numericality_of(:value).is_greater_than(0) }
+
+    it 'common_area_is_mandatory' do
+      single_charge = SingleCharge.new(charge_type: 'common_area_fee')
+
+      single_charge.valid?
+
+      expect(single_charge.errors.include?(:common_area_id)).to be true
+      expect(single_charge.errors.full_messages).to include 'Área Comum deve ser selecionada.'
+    end
+
+    it 'date_is_future' do
+      single_charge = SingleCharge.new(issue_date: 5.days.ago.to_date)
+
+      single_charge.valid?
+
+      expect(single_charge.errors.include?(:issue_date)).to be true
+      expect(single_charge.errors.full_messages).to include 'Data de Emissão deve ser a partir de hoje.'
+    end
+
+    it 'description_is_mandatory' do
+      single_charge = SingleCharge.new(charge_type: 'other')
+      single_charge_two = SingleCharge.new(charge_type: 'fine')
+      single_charge_three = SingleCharge.new(charge_type: 'common_area_fee')
+
+      single_charge.valid?
+      single_charge_two.valid?
+      single_charge_three.valid?
+
+      expect(single_charge.errors.include?(:description)).to be true
+      expect(single_charge.errors.full_messages).to include 'Descrição não pode ficar em branco.'
+      expect(single_charge_two.errors.include?(:description)).to be true
+      expect(single_charge_two.errors.full_messages).to include 'Descrição não pode ficar em branco.'
+      expect(single_charge_three.errors.include?(:description)).to be false
+      expect(single_charge_three.errors.full_messages).not_to include 'Descrição não pode ficar em branco.'
+    end
+
+    it ''
+  end
 end
