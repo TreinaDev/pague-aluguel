309861e04595c2a10511cf9fbbf77079ead512a2
sabinopa <priasabino@gmail.com>
2024-07-11 11:55:47 -0300

    cria classe para calcular taxas e retorna taxas compartilhada referentes ao mes
    Co-authored-by: zutin <gianluccaz@hotmail.com>

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,2 +1,3 @@
 class Bill < ApplicationRecord
+  validates :issue_date, :due_date, :total_value_cents, presence: true
 end
--- spec/jobs/generate_monthly_bill_job_spec.rb
+++ spec/jobs/generate_monthly_bill_job_spec.rb
@@ -86,7 +86,7 @@ RSpec.describe GenerateMonthlyBillJob, type: :job do
       end
     end
 
-    it 'e apenas retorna taxas referentes ao mês atual' do
+    it 'e apenas retorna taxas condominiais referentes ao mês atual' do
       condos = []
       condos << Condo.new(id: 1, name: 'Prédio lindo', city: 'Cidade maravilhosa')
       unit_types = []
@@ -116,6 +116,40 @@ RSpec.describe GenerateMonthlyBillJob, type: :job do
         expect(Bill.first.issue_date).to eq Time.zone.today.beginning_of_month
         expect(Bill.first.due_date).to eq Time.zone.today.beginning_of_month + 9.days
         expect(Bill.first.total_value_cents).to eq 800_00
+      end
+    end
+
+    it 'e apenas retorna taxas compartilhadas referentes ao mês atual' do
+      condos = []
+      condos << Condo.new(id: 1, name: 'Prédio lindo', city: 'Cidade maravilhosa')
+      unit_types = []
+      unit_types << UnitType.new(id: 1, area: 30, description: 'Apartamento 1 quarto', ideal_fraction: 0.1,
+                                 condo_id: 1)
+      units = []
+      units << Unit.new(id: 1, area: 100, floor: 1, number: 1, unit_type_id: 1)
+      first_shared_fee = create(:shared_fee, description: 'Descrição', issue_date: 10.days.from_now.to_date,
+                                             total_value: 20_000_00, condo_id: condos.first.id)
+      second_shared_fee = create(:shared_fee, description: 'Descrição', issue_date: 45.days.from_now.to_date,
+                                              total_value: 13_000_00, condo_id: condos.first.id)
+      create(:shared_fee_fraction, shared_fee: first_shared_fee, unit_id: 1, value_cents: 200_00)
+      create(:shared_fee_fraction, shared_fee: second_shared_fee, unit_id: 1, value_cents: 130_00)
+      first_base_fee = create(:base_fee, condo_id: 1, recurrence: :monthly, charge_day: 10.days.from_now)
+      second_base_fee = create(:base_fee, condo_id: 1, recurrence: :monthly, charge_day: 45.days.from_now)
+      create(:value, price_cents: 150_00, base_fee_id: first_base_fee.id)
+      create(:value, price_cents: 111_11, base_fee_id: second_base_fee.id)
+      allow(Condo).to receive(:all).and_return(condos)
+      allow(Condo).to receive(:find).and_return(condos.first)
+      allow(UnitType).to receive(:find_all_by_condo).and_return(unit_types)
+      allow(Unit).to receive(:find).and_return(units.first)
+      allow(Unit).to receive(:find_all_by_condo).and_return(units)
+
+      travel_to 65.days.from_now do
+        GenerateMonthlyBillJob.perform_now
+
+        expect(Bill.count).to eq 1
+        expect(Bill.first.issue_date).to eq Time.zone.today.beginning_of_month
+        expect(Bill.first.due_date).to eq Time.zone.today.beginning_of_month + 9.days
+        expect(Bill.first.total_value_cents).to eq 241_11
       end
     end
   end
