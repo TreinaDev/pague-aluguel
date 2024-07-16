d46637511da6a52de1155cf2dedeb75c582144ac
kanzakisuemi <kanzakisuemi@edu.unifil.br>
2024-07-15 21:22:17 -0300

    traduz e arruma validação de bills

--- app/models/bill.rb
+++ app/models/bill.rb
@@ -1,3 +1,3 @@
 class Bill < ApplicationRecord
-  validates :issue_date, :due_date, :total_value_cents, presence: true
+  validates :issue_date, :due_date, :total_value_cents, :condo_id, presence: true
 end
