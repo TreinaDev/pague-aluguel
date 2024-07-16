bbf215f8a3b00a6e5902569c1bf11ff6568b69be
kanzakisuemi <kanzakisuemi@edu.unifil.br>
2024-07-15 22:29:12 -0300

    conserta erro do jobs

--- app/jobs/generate_monthly_bill_job.rb
+++ app/jobs/generate_monthly_bill_job.rb
@@ -3,6 +3,8 @@ class GenerateMonthlyBillJob < ApplicationJob
 
   def perform(unit)
     bill = Bill.new(unit_id: unit.id)
+    unit_type = UnitType.find(unit.unit_type_id)
+    bill.condo_id = unit_type.condo_id
     bill.total_value_cents = BillCalculator.calculate_total_fees(unit)
     bill.issue_date = Time.zone.today.beginning_of_month
     bill.due_date = Time.zone.today.beginning_of_month + 9.days
