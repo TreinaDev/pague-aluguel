01a360ba23dc70c30426312f7b2067fce634bddd
kanzakisuemi <kanzakisuemi@edu.unifil.br>
2024-07-17 01:01:35 -0300

    implementa teste para fechar modal de fatura

--- spec/system/bills/user_accesses_associated_bill_spec.rb
+++ spec/system/bills/user_accesses_associated_bill_spec.rb
@@ -101,6 +101,46 @@ describe 'Usuário acessa suas faturas' do
       expect(page).to have_content 10.days.from_now.strftime("%d/%m/%Y")
       expect(page).to have_content number
       expect(page).to have_content 'R$500,00'
+      expect(page).to have_content 'Taxa Condominial'.downcase
+      expect(page).to have_content 'Conta Compartilhada'.downcase
+    end
+
+    it 'e retorna para listagem' do
+      cpf = CPF.generate
+      data = Rails.root.join('spec/support/json/tenant.json').read
+      response = double('response', success?: true, body: data)
+      endpoint_route = "http://127.0.0.1:3000/api/v1/get_tenant_residence?registration_number=#{CPF.new(cpf).formatted}"
+      allow(Faraday).to receive(:get).with(endpoint_route).and_return(response)
+      residence = JSON.parse(response.body)['resident']['residence']
+      resident = JSON.parse(response.body)['resident']
+
+      condo_id = residence['condo_id']
+      condo_name = residence['condo_name']
+      floor = residence['floor']
+      number = residence['number']
+      unit_id = residence['id']
+
+      units = []
+      units << Unit.new(id: unit_id, area: 100, floor: 2, number: 3, unit_type_id: 1)
+
+      allow(Unit).to receive(:find).and_return(units.first)
+
+      bill = create(:bill, condo_id:, unit_id:, issue_date: Time.zone.today.beginning_of_month,
+                           due_date: 10.days.from_now, total_value_cents: 500_00)
+
+      visit root_path
+      within 'form#get_tenant_bill' do
+        cpf.each_char { |char| find(:css, "input[id$='get_tenant_bill']").send_keys(char) }
+        click_on 'Buscar'
+      end
+
+      click_on number.to_s
+      find('#back').click
+
+      expect(page).not_to have_content CPF.new(cpf).formatted
+      expect(page).not_to have_content condo_name
+      expect(page).not_to have_content 'Taxa Condominial'.downcase
+      expect(page).not_to have_content 'Conta Compartilhada'.downcase
     end
   end
 end
