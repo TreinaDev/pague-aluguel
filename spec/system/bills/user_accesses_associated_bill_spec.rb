f2d58542846f9179755e16a76a0df0b995d58064
kanzakisuemi <kanzakisuemi@edu.unifil.br>
2024-07-18 17:22:46 -0300

    implementa visualizacao de status da fatura para morador

--- spec/system/bills/user_accesses_associated_bill_spec.rb
+++ spec/system/bills/user_accesses_associated_bill_spec.rb
@@ -16,7 +16,7 @@ describe 'Usuário acessa suas faturas' do
       unit_id = residence['id']
 
       units = []
-      units << Unit.new(id: unit_id, area: 100, floor: 2, number: 3, unit_type_id: 1)
+      units << Unit.new(id: unit_id, area: 100, floor: 2, number: 23, unit_type_id: 1)
       allow(Unit).to receive(:find).and_return(units.first)
 
       issue_date1 = Time.zone.today.beginning_of_month
@@ -27,11 +27,11 @@ describe 'Usuário acessa suas faturas' do
       due_date3 = 50.days.ago
 
       create(:bill, condo_id:, unit_id:, issue_date: issue_date1,
-                    due_date: due_date1, total_value_cents: 500_00)
+                    due_date: due_date1, total_value_cents: 500_00, status: :pending)
       create(:bill, condo_id:, unit_id:, issue_date: issue_date2,
-                    due_date: due_date2, total_value_cents: 600_00)
+                    due_date: due_date2, total_value_cents: 600_00, status: :awaiting)
       create(:bill, condo_id:, unit_id:, issue_date: issue_date3,
-                    due_date: due_date3, total_value_cents: 700_00)
+                    due_date: due_date3, total_value_cents: 700_00, status: :paid)
 
       visit root_path
       within 'form#get_tenant_bill' do
@@ -47,60 +47,37 @@ describe 'Usuário acessa suas faturas' do
       formatted_due_date3 = due_date3.strftime('%d/%m/%Y')
       expect(page).to have_content 'FATURA'
       expect(page).to have_content resident['name'].upcase
-      expect(page).to have_content "Unidade #{number}", count: 3
-      expect(page).to have_content 'data de emissão', count: 3
-      expect(page).to have_content formatted_issue_date1
-      expect(page).to have_content formatted_issue_date2
-      expect(page).to have_content formatted_issue_date3
-      expect(page).to have_content 'data de vencimento', count: 3
-      expect(page).to have_content formatted_due_date1
-      expect(page).to have_content formatted_due_date2
-      expect(page).to have_content formatted_due_date3
-      expect(page).to have_content 'valor total', count: 3
-      expect(page).to have_content 'R$500,00'
-      expect(page).to have_content 'R$600,00'
-      expect(page).to have_content 'R$700,00'
-    end
 
-    it 'e clica em uma' do
-      cpf = CPF.generate
-      data = Rails.root.join('spec/support/json/tenant.json').read
-      response = double('response', success?: true, body: data)
-      endpoint_route = "http://127.0.0.1:3000/api/v1/get_tenant_residence?registration_number=#{CPF.new(cpf).formatted}"
-      allow(Faraday).to receive(:get).with(endpoint_route).and_return(response)
-      residence = JSON.parse(response.body)['resident']['residence']
-      resident = JSON.parse(response.body)['resident']
-
-      condo_id = residence['condo_id']
-      condo_name = residence['condo_name']
-      number = residence['number']
-      unit_id = residence['id']
-
-      units = []
-      units << Unit.new(id: unit_id, area: 100, floor: 2, number: 3, unit_type_id: 1)
-
-      allow(Unit).to receive(:find).and_return(units.first)
-
-      create(:bill, condo_id:, unit_id:, issue_date: Time.zone.today.beginning_of_month,
-                    due_date: 10.days.from_now, total_value_cents: 500_00)
-
-      visit root_path
-      within 'form#get_tenant_bill' do
-        cpf.each_char { |char| find(:css, "input[id$='get_tenant_bill']").send_keys(char) }
-        click_on 'Buscar'
+      within('a#bill_1') do
+        expect(page).to have_content 'Unidade 23'
+        expect(page).to have_content 'valor total'
+        expect(page).to have_content 'R$500,00'
+        expect(page).to have_content 'data de emissão'
+        expect(page).to have_content formatted_issue_date1
+        expect(page).to have_content 'data de vencimento'
+        expect(page).to have_content formatted_due_date1
+        expect(page).to have_content 'PENDENTE'
       end
-
-      click_on number.to_s
-
-      expect(page).to have_content CPF.new(cpf).formatted
-      expect(page).to have_content resident['name']
-      expect(page).to have_content condo_name
-      expect(page).to have_content Time.zone.today.beginning_of_month.strftime('%d/%m/%Y')
-      expect(page).to have_content 10.days.from_now.strftime('%d/%m/%Y')
-      expect(page).to have_content number
-      expect(page).to have_content 'R$500,00'
-      expect(page).to have_content 'Taxa Condominial'.downcase
-      expect(page).to have_content 'Conta Compartilhada'.downcase
+      within('a#bill_2') do
+        expect(page).to have_content 'Unidade 23'
+        expect(page).to have_content 'valor total'
+        expect(page).to have_content 'R$600,00'
+        expect(page).to have_content 'data de emissão'
+        expect(page).to have_content formatted_issue_date2
+        expect(page).to have_content 'data de vencimento'
+        expect(page).to have_content formatted_due_date2
+        expect(page).to have_content 'AGUARDANDO'
+      end
+      within('a#bill_3') do
+        expect(page).to have_content 'Unidade 23'
+        expect(page).to have_content 'valor total'
+        expect(page).to have_content 'R$700,00'
+        expect(page).to have_content 'data de emissão'
+        expect(page).to have_content formatted_issue_date3
+        expect(page).to have_content 'data de vencimento'
+        expect(page).to have_content formatted_due_date3
+        expect(page).to have_content 'PAGA'
+      end
     end
 
     it 'e retorna para listagem' do
@@ -171,5 +148,139 @@ describe 'Usuário acessa suas faturas' do
 
       expect(page).to have_content I18n.t('views.index.document_number_not_found')
     end
+  end
+end
+describe 'Usuário acessa uma fatura' do
+  it 'pendente' do
+    cpf = CPF.generate
+    data = Rails.root.join('spec/support/json/tenant.json').read
+    response = double('response', success?: true, body: data)
+    endpoint_route = "http://127.0.0.1:3000/api/v1/get_tenant_residence?registration_number=#{CPF.new(cpf).formatted}"
+    allow(Faraday).to receive(:get).with(endpoint_route).and_return(response)
+    residence = JSON.parse(response.body)['resident']['residence']
+    resident = JSON.parse(response.body)['resident']
+
+    condo_id = residence['condo_id']
+    condo_name = residence['condo_name']
+    number = residence['number']
+    unit_id = residence['id']
+
+    units = []
+    units << Unit.new(id: unit_id, area: 100, floor: 2, number: 3, unit_type_id: 1)
+
+    allow(Unit).to receive(:find).and_return(units.first)
+
+    create(:bill, condo_id:, unit_id:, issue_date: Time.zone.today.beginning_of_month,
+                  due_date: 10.days.from_now, total_value_cents: 500_00, status: :pending)
+
+    visit root_path
+    within 'form#get_tenant_bill' do
+      cpf.each_char { |char| find(:css, "input[id$='get_tenant_bill']").send_keys(char) }
+      click_on 'Buscar'
+    end
+
+    click_on number.to_s
+
+    expect(page).to have_content CPF.new(cpf).formatted
+    expect(page).to have_content resident['name']
+    expect(page).to have_content condo_name
+    expect(page).to have_content Time.zone.today.beginning_of_month.strftime('%d/%m/%Y')
+    expect(page).to have_content 10.days.from_now.strftime('%d/%m/%Y')
+    expect(page).to have_content number
+    expect(page).to have_content 'R$500,00'
+    expect(page).to have_content 'Taxa Condominial'.downcase
+    expect(page).to have_content 'Conta Compartilhada'.downcase
+    expect(page).to have_content 'PENDENTE'
+    expect(page).not_to have_button 'Ver comprovante'
+    expect(page).not_to have_button 'Aceitar pagamento'
+    expect(page).not_to have_button 'Recusar pagamento'
+  end
+  it 'aguardando' do
+    cpf = CPF.generate
+    data = Rails.root.join('spec/support/json/tenant.json').read
+    response = double('response', success?: true, body: data)
+    endpoint_route = "http://127.0.0.1:3000/api/v1/get_tenant_residence?registration_number=#{CPF.new(cpf).formatted}"
+    allow(Faraday).to receive(:get).with(endpoint_route).and_return(response)
+    residence = JSON.parse(response.body)['resident']['residence']
+    resident = JSON.parse(response.body)['resident']
+
+    condo_id = residence['condo_id']
+    condo_name = residence['condo_name']
+    number = residence['number']
+    unit_id = residence['id']
+
+    units = []
+    units << Unit.new(id: unit_id, area: 100, floor: 2, number: 3, unit_type_id: 1)
+
+    allow(Unit).to receive(:find).and_return(units.first)
+
+    create(:bill, condo_id:, unit_id:, issue_date: Time.zone.today.beginning_of_month,
+                  due_date: 10.days.from_now, total_value_cents: 500_00, status: :awaiting)
+
+    visit root_path
+    within 'form#get_tenant_bill' do
+      cpf.each_char { |char| find(:css, "input[id$='get_tenant_bill']").send_keys(char) }
+      click_on 'Buscar'
+    end
+
+    click_on number.to_s
+
+    expect(page).to have_content CPF.new(cpf).formatted
+    expect(page).to have_content resident['name']
+    expect(page).to have_content condo_name
+    expect(page).to have_content Time.zone.today.beginning_of_month.strftime('%d/%m/%Y')
+    expect(page).to have_content 10.days.from_now.strftime('%d/%m/%Y')
+    expect(page).to have_content number
+    expect(page).to have_content 'R$500,00'
+    expect(page).to have_content 'Taxa Condominial'.downcase
+    expect(page).to have_content 'Conta Compartilhada'.downcase
+    expect(page).to have_content 'AGUARDANDO'
+    expect(page).to have_button 'Ver comprovante'
+    expect(page).not_to have_button 'Aceitar pagamento'
+    expect(page).not_to have_button 'Recusar pagamento'
+  end
+  it 'paga' do
+    cpf = CPF.generate
+    data = Rails.root.join('spec/support/json/tenant.json').read
+    response = double('response', success?: true, body: data)
+    endpoint_route = "http://127.0.0.1:3000/api/v1/get_tenant_residence?registration_number=#{CPF.new(cpf).formatted}"
+    allow(Faraday).to receive(:get).with(endpoint_route).and_return(response)
+    residence = JSON.parse(response.body)['resident']['residence']
+    resident = JSON.parse(response.body)['resident']
+
+    condo_id = residence['condo_id']
+    condo_name = residence['condo_name']
+    number = residence['number']
+    unit_id = residence['id']
+
+    units = []
+    units << Unit.new(id: unit_id, area: 100, floor: 2, number: 3, unit_type_id: 1)
+
+    allow(Unit).to receive(:find).and_return(units.first)
+
+    create(:bill, condo_id:, unit_id:, issue_date: Time.zone.today.beginning_of_month,
+                  due_date: 10.days.from_now, total_value_cents: 500_00, status: :paid)
+
+    visit root_path
+    within 'form#get_tenant_bill' do
+      cpf.each_char { |char| find(:css, "input[id$='get_tenant_bill']").send_keys(char) }
+      click_on 'Buscar'
+    end
+
+    click_on number.to_s
+
+    expect(page).to have_content CPF.new(cpf).formatted
+    expect(page).to have_content resident['name']
+    expect(page).to have_content condo_name
+    expect(page).to have_content Time.zone.today.beginning_of_month.strftime('%d/%m/%Y')
+    expect(page).to have_content 10.days.from_now.strftime('%d/%m/%Y')
+    expect(page).to have_content number
+    expect(page).to have_content 'R$500,00'
+    expect(page).to have_content 'Taxa Condominial'.downcase
+    expect(page).to have_content 'Conta Compartilhada'.downcase
+    expect(page).to have_content 'PAGA'
+    expect(page).to have_button 'Ver comprovante'
+    expect(page).not_to have_button 'Aceitar pagamento'
+    expect(page).not_to have_button 'Recusar pagamento'
   end
 end
