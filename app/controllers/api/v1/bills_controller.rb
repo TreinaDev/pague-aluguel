2c788091e17b5ec53d03fb38fcd8ebdab890ec82
angelomaia <request.angelo@gmail.com>
2024-07-19 16:47:22 -0300

    Inclui detalhes de contas, taxas e cobranças no API endpoint de detalhes de fatura.
    Co-authored-by: Priscila Sabino <priasabino@gmail.com>

--- app/controllers/api/v1/bills_controller.rb
+++ app/controllers/api/v1/bills_controller.rb
@@ -18,7 +18,12 @@ class Api::V1::BillsController < Api::V1::ApiController
   def bill_json(bill)
     { unit_id: bill.unit_id, condo_id: bill.condo_id, issue_date: bill.issue_date, due_date: bill.due_date,
       total_value_cents: bill.total_value_cents, status: bill.status,
-      values: { base_fee_value_cents: bill.base_fee_value_cents, shared_fee_value_cents: bill.shared_fee_value_cents,
-                single_charge_value_cents: bill.single_charge_value_cents, rent_fee_cents: bill.rent_fee_cents } }
+      values: {
+        base_fee_value_cents: bill.base_fee_value_cents, shared_fee_value_cents: bill.shared_fee_value_cents,
+        single_charge_value_cents: bill.single_charge_value_cents, rent_fee_cents: bill.rent_fee_cents
+      },
+      bill_details: bill.bill_details.map do |detail|
+                      { value_cents: detail.value_cents, description: detail.description, fee_type: detail.fee_type }
+                    end }
   end
 end
