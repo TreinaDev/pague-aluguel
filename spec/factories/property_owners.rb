aaf991dd20d9b00c58c10a125e01de62cdc05097
kanzakisuemi <kanzakisuemi@edu.unifil.br>
2024-07-19 18:27:48 -0300

    corrige testes relacionados ao proprietário

--- spec/factories/property_owners.rb
+++ spec/factories/property_owners.rb
@@ -2,6 +2,8 @@ FactoryBot.define do
   factory :property_owner do
     email { 'nakedsnake@mgs.com' }
     password { 'bigboss' }
+    first_name { 'Severus' }
+    last_name { 'Snake' }
     document_number { CPF.generate }
   end
 end
