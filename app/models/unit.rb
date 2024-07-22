ee2bd648cee7f858bf16644f5ea8e7a3d4d86295
kanzakisuemi <kanzakisuemi@edu.unifil.br>
2024-07-22 01:06:55 -0300

    recebe nome da torre api condominions

--- app/models/unit.rb
+++ app/models/unit.rb
@@ -1,5 +1,5 @@
 class Unit
-  attr_accessor :id, :area, :floor, :number, :unit_type_id, :condo_id, :condo_name, :tenant_id, :owner_id, :description
+  attr_accessor :id, :area, :floor, :number, :unit_type_id, :condo_id, :condo_name, :tenant_id, :owner_id, :description, :tower_name
 
   def initialize(attribute = {})
     @id = attribute[:id]
@@ -12,6 +12,7 @@ class Unit
     @tenant_id = attribute[:tenant_id]
     @owner_id = attribute[:owner_id]
     @description = attribute[:description]
+    @tower_name = attribute[:tower_name]
   end
 
   def self.all(condo_id)
@@ -96,6 +97,6 @@ class Unit
   def self.build_new_unit(data)
     Unit.new(id: data['id'], area: data['area'], floor: data['floor'], number: data['number'],
              unit_type_id: data['unit_type_id'], condo_id: data['condo_id'], condo_name: data['condo_name'],
-             tenant_id: data['tenant_id'], owner_id: data['owner_id'], description: data['description'])
+             tenant_id: data['tenant_id'], owner_id: data['owner_id'], description: data['description'], tower_name: data['tower_name'])
   end
 end
